56490d51667bd39d0f1855a22a9f190f0b7aa0c3
Watchanon Numnam <jiramot@gmail.com>
2018-01-26 15:54:32 +0700

    Retrieve user context in controller

--- src/main/java/com/jiramot/auth/resource/hello/HelloController.java
+++ src/main/java/com/jiramot/auth/resource/hello/HelloController.java
@@ -1,12 +1,18 @@
 package com.jiramot.auth.resource.hello;
 
+import com.jiramot.auth.resource.security.UserContext;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
   @GetMapping("/")
-  public String hello() {
-    return "hello";
+  public String hello(Authentication auth) {
+    auth.getPrincipal()
+
+    return "hello " + ((UserContext) auth.getPrincipal()).getUsername();
+  }
   }
 }
